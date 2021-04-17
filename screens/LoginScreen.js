2f82e0224b7c14f6d3f0dfe2fdc2aa9a0dc7da1d
Rebecca Hong <rebeccahongsf@gmail.com>
2021-04-16 23:18:37 -0700

    style(login and registration input): add icons and styles to login and registration input fields

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase';
 import React, { useState } from 'react';
-import { View, TextInput, Button, Text } from 'react-native';
+import { View, TextInput, Button, Text, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const LoginScreen = (props) => {
   const [email, setEmail] = useState('');
@@ -21,20 +22,51 @@ const LoginScreen = (props) => {
   };
 
   return (
-    <View>
-      <Text>Login</Text>
-      <TextInput
-        placeholder="email"
-        onChangeText={(email) => setEmail(email)}
-      />
-      <TextInput
-        placeholder="password"
-        secureTextEntry={true}
-        onChangeText={(password) => setPassword(password)}
-      />
-      <Button onPress={() => onLogin()} title="Login" />
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <Icon style={styles.icon} name="ios-mail" size={25} color="#4F8EF7" />
+        <TextInput
+          placeholder="email"
+          onChangeText={(email) => setEmail(email)}
+          style={styles.inputText}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Icon style={styles.icon} name="ios-key" size={25} color="#4F8EF7" />
+        <TextInput
+          placeholder="password"
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+          style={styles.inputText}
+        />
+      </View>
+      <View style={styles.button}>
+        <Button onPress={() => onLogin()} title="Login" color="#fff" />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 30,
+  },
+  inputContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginBottom: 25,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  inputText: {
+    fontSize: 16,
+  },
+  button: {
+    backgroundColor: '#ccc',
+    padding: 5,
+    width: '100%',
+  },
+});
 
 export default LoginScreen;
--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase';
 import React, { useState } from 'react';
-import { View, TextInput, Button, Text } from 'react-native';
+import { View, TextInput, Button, Text, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const RegisterScreen = (props) => {
   const [email, setEmail] = useState('');
@@ -21,21 +22,55 @@ const RegisterScreen = (props) => {
   };
 
   return (
-    <View>
-      <Text>Register</Text>
-      <TextInput placeholder="name" onChangeText={(name) => setName(name)} />
-      <TextInput
-        placeholder="email"
-        onChangeText={(email) => setEmail(email)}
-      />
-      <TextInput
-        placeholder="password"
-        secureTextEntry={true}
-        onChangeText={(password) => setPassword(password)}
-      />
-      <Button onPress={() => onSignUp()} title="Sign Up" />
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <Icon style={styles.icon} name="ios-person" size={25} color="#4F8EF7" />
+        <TextInput placeholder="name" onChangeText={(name) => setName(name)} />
+      </View>
+      <View style={styles.inputContainer}>
+        <Icon style={styles.icon} name="ios-mail" size={25} color="#4F8EF7" />
+        <TextInput
+          placeholder="email"
+          onChangeText={(email) => setEmail(email)}
+          style={styles.inputText}
+        />
+      </View>
+      <View style={styles.inputContainer}>
+        <Icon style={styles.icon} name="ios-key" size={25} color="#4F8EF7" />
+        <TextInput
+          placeholder="password"
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+          style={styles.inputText}
+        />
+      </View>
+      <View style={styles.button}>
+        <Button onPress={() => onSignUp()} title="Sign Up" color="#fff" />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 30,
+  },
+  inputContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginBottom: 25,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  inputText: {
+    fontSize: 16,
+  },
+  button: {
+    backgroundColor: '#ccc',
+    padding: 5,
+    width: '100%',
+  },
+});
 
 export default RegisterScreen;
